0f8924bf7f337893af6af166452a63e7a262f79e
Tata Lee <67614323+punkzappa007@users.noreply.github.com>
2021-12-22 19:10:19 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 # Just a basic script U can improvise lateron asper ur need xD 
 
-MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11"
+MANIFEST="git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-11.0"
 DEVICE=CG8
-DT_LINK="https://github.com/punkzappa007/android_device_tecno_TECNO-CG8.git -b android-11.0"
+DT_LINK="https://github.com/punkzappa007/android_device_umidigi_a9pro -b CG8-PBRP"
 DT_PATH=device/tecno/CG8
 
 echo " ===+++ Setting up Build Environment +++==="
-apt install openssh-server -y
-apt update --fix-missing
-apt install openssh-server -y
-mkdir ~/twrp11 && cd ~/twrp11
+sudo -E apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php* 
+sudo -E apt-get clean 
+sudo -E apt-get -qq update
+sudo -E apt-get -qq install bc build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev repo rsync flex curl  bison aria2
+sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
+sudo chmod a+rx /usr/local/bin/repo
 
 echo " ===+++ Syncing Recovery Sources +++==="
-repo init --depth=1 -u $MANIFEST
-repo sync
-git clone --depth=1 $DT_LINK $DT_PATH
+mkdir work
+cd work
+repo init -u $MANIFEST --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips
+repo sync -j4
+git clone $DT_LINK --depth=1 --single-branch $DT_PATH
 
 echo " ===+++ Building Recovery +++==="
+cd work
 . build/envsetup.sh
 export TW_THEME=portrait_hdpi
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch twrp_CG8-eng && mka bootimage
+lunch twrp_${DEVICE}-eng && mka bootimage
 
-# Upload zips & recovery.img (U can improvise lateron adding telegram supportetc etc)
+# Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
 echo " ===+++ Uploading Recovery +++==="
 version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
 OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
 
 cd out/target/product/$DEVICE
-mv boot.img ${OUTFILE%.zip}.img
+mv recovery.img ${OUTFILE%.zip}.img
 zip -r9 $OUTFILE ${OUTFILE%.zip}.img
 
-##curl -T $OUTFILE https://oshi.at
+#curl -T $OUTFILE https://oshi.at
 curl -sL $OUTFILE https://git.io/file-transfer | sh
 ./transfer wet *.zip

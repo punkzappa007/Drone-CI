eaec56083616ca0c56fcb226a2aee20e5e975baf
Tata Lee <67614323+punkzappa007@users.noreply.github.com>
2022-01-14 13:03:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,28 +7,23 @@ DT_LINK="https://github.com/punkzappa007/android_device_tecno_CG8-PBRP.gi -b CG8
 DT_PATH=device/tecno/CG8
 
 echo " ===+++ Setting up Build Environment +++==="
-sudo -E apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php* 
-sudo -E apt-get clean 
-sudo -E apt-get -qq update
-sudo -E apt-get -qq install bc build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev repo rsync flex curl  bison aria2
-sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
-sudo chmod a+rx /usr/local/bin/repo
+apt install openssh-server -y
+apt update --fix-missing
+apt install openssh-server -y
+mkdir ~/twrp11 && cd ~/twrp11
 
-echo " ===+++ Syncing Recovery Sources +++==="
-mkdir work
-cd work
-repo init -u $MANIFEST --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips
-repo sync -j4
-git clone $DT_LINK --depth=1 --single-branch $DT_PATH
+echo " ===+++ Syncing Recovery Sources  +++==="
+repo init --depth=1 -u $MANIFEST
+repo sync
+git clone --depth=1 $DT_LINK $DT_PATH
 
 echo " ===+++ Building Recovery +++==="
-cd work
 . build/envsetup.sh
 export TW_THEME=portrait_hdpi
 export ALLOW_MISSING_DEPENDENCIES=true
 lunch omni_${DEVICE}-eng && mka bootimage
 
-# Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
+# Upload zips & recovery.img (U can improvise lateron adding telegram supportetc etc)
 echo " ===+++ Uploading Recovery +++==="
 version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
 OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip

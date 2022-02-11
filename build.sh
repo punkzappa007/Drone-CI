3134a41004e14342e2239be9cfc31a1ecbeb5629
Tata Lee <67614323+punkzappa007@users.noreply.github.com>
2022-02-11 19:57:15 +0800

    OFOX

--- build.sh
+++ build.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 # Just a basic script U can improvise lateron asper ur need xD
 
-MANIFEST="git://github.com/SHRP/manifest.git -b v3_11.0"
+MANIFEST="https://gitlab.com/OrangeFox/sync.git"
 DEVICE=CG8
-DT_LINK="https://github.com/punkzappa007/android_device_tecno_TECNO-CG8.git -b CG8-SHRP"
+DT_LINK="https://github.com/punkzappa007/android_device_tecno_TECNO-CG8.git -b OFOX-CG8"
 DT_PATH=device/tecno/CG8
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
 apt update --fix-missing
 apt install openssh-server -y
-mkdir ~/twrp11 && cd ~/twrp11
+mkdir mkdir ~/OrangeFox_sync && cd ~/OrangeFox_sync
 
 echo " ===+++ Syncing Recovery Sources  +++==="
-repo init --depth=1 -u $MANIFEST
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-#repo sync
+git clone $MANIFEST
+cd ~/OrangeFox_sync/sync/
+./orangefox_sync.sh --branch 11.0 --path ~/fox_11.0
+cd ~/fox_11.0
 git clone --depth=1 $DT_LINK $DT_PATH
 
 echo " ===+++ Building Recovery +++==="
+cd ~/fox_11.0
 . build/envsetup.sh
 export TW_THEME=portrait_hdpi
 export ALLOW_MISSING_DEPENDENCIES=true
@@ -29,7 +31,7 @@ echo " ===+++ Uploading Recovery +++==="
 version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
 OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
 
-cd out/target/product/$DEVICE
+cd ~/work/out/target/product/$DEVICE
 mv boot.img ${OUTFILE%.zip}.img
 zip -r9 $OUTFILE ${OUTFILE%.zip}.img
 

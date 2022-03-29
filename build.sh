e974044a50ecd2d7eaa91181d38002cddcd261f4
Tata Lee <67614323+punkzappa007@users.noreply.github.com>
2022-03-29 22:02:39 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Just a basic script U can improvise lateron asper ur need xD 
+# Just a basic script U can improvise lateron asper ur need xD
 
 MANIFEST="git://github.com/PitchBlackRecoveryProject/manifest_pb -b android-11.0"
-DT_LINK="https://github.com/punkzappa007/android_device_tecno_CG8-PBRP.git -b CG8-Pbrp"
+DT_LINK="https://github.com/punkzappa007/android_device_tecno_TECNO-CG8.git -b PBRP-CG8"
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
@@ -11,13 +11,15 @@ apt install openssh-server -y
 mkdir ~/twrp11 && cd ~/twrp11
 
 echo " ===+++ Syncing Recovery Sources  +++==="
-repo init --depth=1 -u git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-11.0 --groups=all,-notdefault,-device,-darwin,-x86,-mips
-#repo init -u https://github.com/PitchBlackRecoveryProject/manifest_pb -b android-11.0
+#==================working=========
+#repo init --depth=1 -u git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-11.0 --groups=all,-notdefault,-device,-darwin,-x86,-mips
+#==================================
+repo init -u https://github.com/PitchBlackRecoveryProject/manifest_pb -b android-11.0
 repo sync --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
 
 #repo init --depth=1 -u $MANIFEST
 #repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-git clone --depth=1 $DT_LINK device/TECNO/CG8
+git clone --depth=1 $DT_LINK device/tecno/CG8
 
 echo " ===+++ Building Recovery +++==="
 . build/envsetup.sh
@@ -25,7 +27,7 @@ export TW_THEME=portrait_hdpi
 export ALLOW_MISSING_DEPENDENCIES=true
 #lunch omni_cg8-eng && mka pbrp
 lunch omni_CG8-eng && mka -j$(nproc --all) pbrp
-# Upload zips & recovery.img (U can improvise lateron adding telegram supportetc etc)
+# Upload zips & recovery.img (U can improvise lateron adding telegram supportetc etc) 
 echo " ===+++ Uploading Recovery +++==="
 cd out/target/product/CG8
 

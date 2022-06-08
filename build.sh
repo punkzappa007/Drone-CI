d7214e40dc8e9decc1e95344333568bd2aa610a2
Tata Lee <67614323+punkzappa007@users.noreply.github.com>
2022-06-08 20:56:52 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ export TW_THEME=portrait_hdpi
 export ALLOW_MISSING_DEPENDENCIES=true
 #lunch omni_cg8-eng && mka pbrp
 lunch omni_CG8-eng && mka -j$(nproc --all) pbrp
-# Upload zips & recovery.img (U can improvise lateron adding telegram supportetc etc) 
+# Upload zips & recovery.img (U can improvise lateron adding telegram supportetc etc)
 echo " ===+++ Uploading Recovery +++==="
 cd out/target/product/CG8
 

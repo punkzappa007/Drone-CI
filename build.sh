4a481a9f9730bed6d14a472b8c4e0d6590cd1f74
Tata Lee <67614323+punkzappa007@users.noreply.github.com>
2022-06-08 14:55:40 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Just a basic script U can improvise lateron asper ur need xD
 
-MANIFEST="git://github.com/PitchBlackRecoveryProject/manifest_pb -b android-11.0"
-DT_LINK="https://github.com/punkzappa007/android_device_tecno_TECNO-CG8.git -b PBRP-CG8"
+MANIFEST="git://github.com/PitchBlackRecoveryProject/manifest_pb -b android-9.0"
+DT_LINK="https://github.com/PitchBlackRecoveryProject/android_device_xiaomi_pine-pbrp.git -b android-9.0"
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
@@ -14,24 +14,24 @@ echo " ===+++ Syncing Recovery Sources  +++==="
 #==================working=========
 #repo init --depth=1 -u git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-11.0 --groups=all,-notdefault,-device,-darwin,-x86,-mips
 #==================================
-repo init -u https://github.com/PitchBlackRecoveryProject/manifest_pb -b android-11.0
+repo init -u https://github.com/PitchBlackRecoveryProject/manifest_pb -b android-9.0
 repo sync --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
 
 #repo init --depth=1 -u $MANIFEST
 #repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-git clone --depth=1 $DT_LINK device/tecno/CG8
+git clone --depth=1 $DT_LINK device/xiaomi/pine
 
 echo " ===+++ Building Recovery +++==="
 . build/envsetup.sh
 export TW_THEME=portrait_hdpi
 export ALLOW_MISSING_DEPENDENCIES=true
 #lunch omni_cg8-eng && mka pbrp
-lunch omni_CG8-eng && mka -j$(nproc --all) pbrp
+lunch omni_pine-eng && mka -j$(nproc --all) pbrp
 # Upload zips & recovery.img (U can improvise lateron adding telegram supportetc etc) 
 echo " ===+++ Uploading Recovery +++==="
-cd out/target/product/CG8
+cd out/target/product/pine
 
-sudo zip -r9 PBRP-CG8.zip recovery.img
+sudo zip -r9 PBRP-pine.zip recovery.img
 
 curl -sL https://git.io/file-transfer | sh 
 

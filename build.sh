d3b008931cb3e5837d823c93a7db78df4ecf459d
Tata Lee <67614323+punkzappa007@users.noreply.github.com>
2022-06-24 20:57:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Just a basic script U can improvise lateron asper ur need xD
 
 MANIFEST="git://github.com/PitchBlackRecoveryProject/manifest_pb -b android-11.0"
-DT_LINK="https://github.com/punkzappa007/android_device_tecno_TECNO-CG8.git -b PBRP-CG8"
+DT_LINK="https://github.com/punkzappa007/TECNO-CG8.git -b android-11.0"
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
@@ -11,20 +11,22 @@ apt install openssh-server -y
 mkdir ~/twrp11 && cd ~/twrp11
 
 echo " ===+++ Syncing Recovery Sources  +++==="
-repo init --depth=1 -u git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-11.0 --groups=all,-notdefault,-device,-darwin,-x86,-mips
-repo sync --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+#==================working=========
+#repo init --depth=1 -u git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-11.0 --groups=all,-notdefault,-device,-darwin,-x86,-mips
+#==================================
+repo init -u https://github.com/PitchBlackRecoveryProject/manifest_pb -b android-11.0
 
-#repo init --depth=1 -u $MANIFEST
-#repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-git clone --depth=1 $DT_LINK device/tecno/CG8
+repo sync -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j$(nproc --all)
+
+git clone --depth=1 $DT_LINK device/TECNO/CG8
 
 echo " ===+++ Building Recovery +++==="
 . build/envsetup.sh
 export TW_THEME=portrait_hdpi
 export ALLOW_MISSING_DEPENDENCIES=true
-#lunch omni_cg8-eng && mka pbrp
-lunch omni_CG8-eng && mka -j$(nproc --all) pbrp
-# Upload zips & recovery.img (U can improvise lateron adding telegram supportetc etc) 
+
+lunch twrp_CG8-eng && mka -j$(nproc --all) pbrp
+# Upload zips & recovery.img (U can improvise lateron adding telegram supportetc etc)
 echo " ===+++ Uploading Recovery +++==="
 cd out/target/product/CG8
 
